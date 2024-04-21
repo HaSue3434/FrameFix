d4887ce30496dcfb5faa97883b04476d90fd751e
HaSue3434 <tlsgur5669@gmail.com>
2024-04-21 21:04:19 +0900

    수정

--- project/backend/ProjectFr/src/main/java/framefix/com/ProjectFr/ProjectFrApplication.java
+++ project/backend/ProjectFr/src/main/java/framefix/com/ProjectFr/ProjectFrApplication.java
@@ -14,5 +14,5 @@ public class ProjectFrApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectFrApplication.class, args);
 	}
-
+	
 }
--- project/src/assets/pages/sign-up/sign.tsx
+++ project/src/assets/pages/sign-up/sign.tsx
@@ -3,14 +3,41 @@ import Styles from "./sign.module.css";
 import { ReactComponent as Logo } from "../../img/logo/logo.svg";
 import { ReactComponent as Google } from "../../img/icons/google-icon.svg";
 import { ReactComponent as ThreePath } from "../../img/icons/3d-path.svg";
+import { ReactComponent as FLogo } from "../../img/logo/frame-logo.svg";
 
-import { useGoogleLogin,
+import { ReactComponent as BottomArrow } from "../../img/icons/framefix-plugin-icons/aseets-arrow-bottom.svg";
+import { ReactComponent as Cursor } from "../../img/icons/framefix-plugin-icons/cursor.svg";
+import { ReactComponent as Frame } from "../../img/icons/framefix-plugin-icons/frame.svg";
+import { ReactComponent as Rect } from "../../img/icons/framefix-plugin-icons/Rect.svg";
+import { ReactComponent as Hand } from "../../img/icons/framefix-plugin-icons/hand.svg";
+import { ReactComponent as TextCanvas } from "../../img/text-canvas.svg";
+
+/* framefix icon*/
+import { ReactComponent as Template } from "../../img/icons/framefix-plugin-icons/template.svg";
+import { ReactComponent as Layer } from "../../img/icons/framefix-plugin-icons/layer.svg";
+import { ReactComponent as Text } from "../../img/icons/framefix-plugin-icons/text.svg";
+import { ReactComponent as Menus } from "../../img/icons/framefix-plugin-icons/menus.svg";
+import { ReactComponent as Header } from "../../img/icons/framefix-plugin-icons/header.svg";
+import { ReactComponent as Footer } from "../../img/icons/framefix-plugin-icons/footer.svg";
+import { ReactComponent as Slides } from "../../img/icons/framefix-plugin-icons/slides.svg";
+import { ReactComponent as Button } from "../../img/icons/framefix-plugin-icons/button.svg";
+import { ReactComponent as Icons } from "../../img/icons/framefix-plugin-icons/icons.svg";
+import { ReactComponent as Magic } from "../../img/icons/framefix-plugin-icons/magic.svg";
+
+/* framefix icon*/ 
+
+import { motion } from 'framer-motion';
+
+import { 
+
+    useGoogleLogin,
     GoogleLogin,
     GoogleLoginProps, 
     GoogleCredentialResponse,
     GoogleOAuthProvider
     
 } from '@react-oauth/google';
+
 import { jwtDecode } from 'jwt-decode'; 
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
@@ -70,7 +97,11 @@ const Sign = (): JSX.Element | null =>{
 
     return (
         <> 
-        <div className={Styles.signInContainer}>
+        <motion.div 
+        initial = {{opacity : 0}}
+        animate =  {{opacity : 1}}
+        transition={{duration : 1, ease : "anticipate"}}
+        className={Styles.signInContainer}>
 
             <div className={Styles.wrapper}>
                 <div className={Styles.signin}>
@@ -109,7 +140,45 @@ const Sign = (): JSX.Element | null =>{
                 <div className={Styles.views}>
                     <div className={Styles.canvas}><ThreePath/></div>
                     <div className={Styles.viewFrameFix}>
-                        <div className={Styles.head}></div>
+                        <div className={Styles.head}>
+                            <div className={Styles.projectName}>
+                                <div className={Styles.fLogo}><FLogo/></div>
+                                <div className={Styles.name}>
+                                    <p><span>Drafts / </span>Project name <BottomArrow/></p>
+                                </div>
+                            </div>
+                            <div className={Styles.tools}>
+                                <div className={`${Styles.icon} ${Styles.selected}`}><Cursor/></div>
+                                <div className={Styles.icon}><Frame/><BottomArrow/></div>
+                                <div className={Styles.icon}><Rect/><BottomArrow/></div>
+                                <div className={Styles.icon}><Hand/></div>
+                            </div>
+                            <div className={Styles.public}></div>
+                        </div>
+                        <div className={Styles.contents}>
+                            <div className={Styles.sideAssetsContain}>
+                                <div className={Styles.plugin}> <div className={Styles.icon}><Template/></div> <p>Template</p> </div>
+                                <div className={Styles.plugin}> <div className={Styles.icon}><Layer/></div> <p>Layer</p> </div>
+                                <div className={Styles.plugin}> <div className={Styles.icon}><Text/></div> <p>Text</p> </div>
+                                <div className={Styles.plugin}> <div className={Styles.icon}><Menus/></div> <p>Menus</p> </div>
+                                <div className={Styles.plugin}> <div className={Styles.icon}><Header/></div> <p>Header</p> </div>
+                                <div className={Styles.plugin}> <div className={Styles.icon}><Footer/></div> <p>Footer</p> </div>
+                                <div className={Styles.plugin}> <div className={Styles.icon}><Slides/></div> <p>Slides</p> </div>
+                                <div className={Styles.plugin}> <div className={Styles.icon}><Button/></div> <p>Button</p> </div>
+                                <div className={Styles.plugin}> <div className={Styles.icon}><Icons/></div> <p>Icons</p> </div>
+                                <div className={Styles.plugin}> <div className={Styles.icon}><Magic/></div> <p>Magic</p> </div>
+                            </div>
+                            <div className={Styles.content}>
+                                <div className={Styles.fBox}>
+                                    <div className={Styles.textCanvas}></div>
+                                    <div className={Styles.item}></div>
+                                </div>
+                                <div className={Styles.sBox}>
+                                    <div className={Styles.sItem}></div>
+                                    <div className={Styles.sItem}></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className={Styles.b}></div>
@@ -124,7 +193,7 @@ const Sign = (): JSX.Element | null =>{
         
         */}
 
-        </div>
+        </motion.div>
         </>
     );
 }

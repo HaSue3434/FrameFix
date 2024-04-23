74aac82e3a6c7c452fd0d7828313e29eff1e3158
HaSue3434 <tlsgur5669@gmail.com>
2024-04-23 23:09:58 +0900

    수정

---
+++ project/backend/ProjectFr/src/main/java/framefix/com/ProjectFr/OAuthController.java
@@ -0,0 +1,8 @@
+package framefix.com.ProjectFr;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OAuthController {
+    
+}
--- project/backend/ProjectFr/src/main/java/framefix/com/ProjectFr/ProjectFrApplication.java
+++ project/backend/ProjectFr/src/main/java/framefix/com/ProjectFr/ProjectFrApplication.java
@@ -6,22 +6,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @SpringBootApplication
 public class ProjectFrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectFrApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFrApplication.class, args);
+    }
 
+    
     @Bean
-    WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000");
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
-	
+    
 }
+

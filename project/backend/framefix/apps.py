b0d62e3bc64e71e02cd427c8be1b3a3a59631405
HaSue3434 <89263456+HaSue3434@users.noreply.github.com>
2024-04-25 11:15:46 +0900

    Delete project/backend directory
    Signed-off-by: HaSue3434 <89263456+HaSue3434@users.noreply.github.com>

--- project/backend/framefix/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FramefixConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'framefix'
--- project/backend/framefix/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import simple_api
-
-urlPatterns = [
-    path('hello/', simple_api),
-]

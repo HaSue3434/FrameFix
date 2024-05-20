233d0288dee57052ebf4770cdf39f82bc4538646
HaSue3434 <tlsgur5669@gmail.com>
2024-05-20 14:33:41 +0900

    수정

--- project/src/assets/pages/framefix-tool/styles-panel/selected-element/el-seleted.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const elSelected = () =>{
-
-}
-export default elSelected;
--- project/src/assets/pages/framefix-tool/styles-panel/tabDesign.tsx
+++ project/src/assets/pages/framefix-tool/styles-panel/tabDesign.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Styles from './stylesPanel.module.css';
 
+import Default from './selected-element/default';
+import frameSelected from './selected-element/frame-seleted';
+import ShapeSelected from "./selected-element/shapeSelected";
+import TextSelected from './selected-element/t-selected';
 
 
 export const tabDesign = () =>{
